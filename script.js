8f67d50a20c6cd7b40aef4743e235e9ac445c44e
Iuliia Goriun <goriunyuliya@gmail.com>
2022-12-14 13:12:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,118 @@
 // Напиши функцію calculateTotalPrice(productName), яка приймає один параметр productName - назва товару. Функція повинна повернути загальну вартість (ціна * кількість) товару з таким ім'ям з масиву products.
 const products = [
-    { name: "Radar", price: 1300, quantity: 4 },
-    { name: "Scanner", price: 2700, quantity: 3 },
-    { name: "Droid", price: 400, quantity: 7 },
-    { name: "Grip", price: 1200, quantity: 9 },
-  ];
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function calculateTotalPrice(productName) {
+  // Change code below this line
+  for (const product of products) {
+    let totalPrice = 0;
+    if (product.name === productName) {
+      totalPrice = product.price * product.quantity;
+
+      return totalPrice;
+    }
+  }
+  // Change code above this line
+
+  return 0;
+
+  // Change code above this line
+}
+
+//  Ми отримали прогноз погоди на два дні, з мінімальними і максимальними температурами, а також необов'язковими іконками. Заміни оголошення всіх змінних однією операцією деструктуризації властивостей об'єкта forecast. Задай значення за замовчуванням для іконок, змінних todayIcon і tomorrowIcon - рядок "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg".
+const forecast = {
+  today: {
+    low: 28,
+    high: 32,
+    icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+  },
+  tomorrow: {
+    low: 27,
+    high: 31,
+  },
+};
+// Change code below this line
+const {
+  today: {
+    low: lowToday,
+    high: highToday,
+    icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+  },
+  tomorrow: {
+    low: lowTomorrow,
+    high: highTomorrow,
+    icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+  },
+} = forecast;
+
+//   У змінних firstGroupScores, secondGroupScores і thirdGroupScores зберігаються результати тестування окремих груп. Використовуючи розподіл, доповни код таким чином, щоб:
+
+const firstGroupScores = [64, 42, 93];
+const secondGroupScores = [89, 14, 51, 26];
+const thirdGroupScores = [29, 47, 18, 97, 81];
+// Change code below this line
+const allScores = [
+  ...firstGroupScores,
+  ...secondGroupScores,
+  ...thirdGroupScores,
+];
+const bestScore = Math.max(...allScores);
+const worstScore = Math.min(...allScores);
+
+
+
+
+// Використовуючи операцію rest, доповни код функції add() таким чином, щоб вона приймала будь-яку кількість аргументів, рахувала і повертала їх суму.
+function add(...args) {
+    let argSum = 0;
+    for( const arg of args) {
+      argSum += arg;
+      
+    } return argSum;
+     
+    // Change code above this line
+  } 
+
+
+
+//   function findMatches(nums, ...args) {
+  const matches = []; // Don't change this line
+  for(arg of args) {
+    for (num of nums) {
+    if (num === arg) {
+      matches.push(num);
+    }
+  }
+  }
+    // Change code above this line
+    return matches;
   
-  function calculateTotalPrice(productName) {
+
+
+//     Додай об'єкту bookShelf ще два методи, які поки що будуть повертати просто рядки за аналогією з getBooks() і addBook(bookName).
+
+// Метод removeBook(bookName) буде видаляти книгу за назвою. Повертає рядок "Deleting book <назва книги>", де <назва книги> - це значення параметра bookName.
+
+// Метод updateBook(oldName, newName) буде оновлювати назву книги на нову. Повертає рядок "Updating book <стара назва> to <нова назва>", де <стара назва> і <нова назва> - це значення параметрів oldName і newName відповідно.
+// //    
+const bookShelf = {
     // Change code below this line
-    for(const product of products) {
-        let totalPrice = 0;
-         if (product.name === productName) {
-             totalPrice = product.price * product.quantity;
-             
-            return totalPrice;
-         }
-        }
-                 // Change code above this line
-       
-      return 0;
-     
-       // Change code above this line
-     }
-     
+    books: ["The last kingdom", "The guardian of dreams"],
+    getBooks() {
+      return "Returning all books";
+    },
+    removeBook(bookName) {
+      return `Deleting book ${bookName}`;
+    },
+    updateBook(oldName, newName) {
+      return `Updating book ${oldName} to ${newName}`;
+    },
+    addBook(bookName) {
+      return `Adding book ${bookName}`;
+    },
+    // Change code above this line
+  };
